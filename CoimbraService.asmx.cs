fd41593ed748859519096550194aa7536c35687d
cabordelu <cabordelu@gmail.com>
2017-12-04 13:12:16 +0000

    Prueba publicacion 2

---
+++ CoimbraService.asmx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+
+namespace CoimbraService
+{
+    /// <summary>
+    /// Descripción breve de Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return (a + b);
+        }
+
+        [WebMethod]
+        public System.Single Subtract(System.Single A, System.Single B)
+        {
+            return (A - B);
+        }
+
+        [WebMethod]
+        public System.Single Multiply(System.Single A, System.Single B)
+        {
+            return A * B;
+        }
+
+        [WebMethod]
+        public System.Single Divide(System.Single A, System.Single B)
+        {
+            if (B == 0)
+                return -1;
+            return Convert.ToSingle(A / B);
+        }
+    }
+}
